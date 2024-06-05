b6bd75fa69d956cde383ded790ec39c8cde6838e
PrangellPlays <prangellplays@gmail.com>
2024-06-05 18:04:13 +0100

    Initial commit

---
+++ src/main/java/dev/prangellplays/prangellsarmoury/PrangellsArmouryDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.prangellplays.prangellsarmoury;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PrangellsArmouryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
